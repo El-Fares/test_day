b2a394bbe24b1a67ba3a5a2a8a5e744527f3ef54
mohamed <mrt301449@gmail.com>
2018-06-05 06:27:51 +0200

    massege_of_day.py

---
+++ massege_of_day.py
@@ -0,0 +1,11 @@
+from random import randint
+
+massege = ["Love is blind; friendship closes its eyes.",
+           "A friend in need is a damned nuisance.",
+           "Ah, how good it feels the hand of an old friend.",
+           "Blind to all faults to a friend one must close one eye: to keep him two.",
+           "The truth in friendship is to me every bit as sacred as eternal marriage.",
+           "What do we live for, if it is not to make life less difficult for each other.",
+           "When a friend asks there is no tomorrow."]
+print massege[randint(0,6)]
+   
